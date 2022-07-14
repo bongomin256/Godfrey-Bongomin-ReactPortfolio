11f7eb5fe800e22173aaff384b1f45289add02ac
Godfrey Bongomin <bongomin@uw.edu>
2022-07-14 18:20:25 -0400

    Deploying to github pages

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>
+          Godfrey <span>Bongomin</span>
+        </h1>
+
+        <nav>
+          <ul>
+            <li>
+              <a href="#about-me">About Me</a>
+            </li>
+            <li>
+              <a href="#mywork">My Work</a>
+            </li>
+            <li>
+              <a href="#contactme">Contact Me</a>
+            </li>
+            <li>
+              <a
+                id="resume"
+                href="https://drive.google.com/file/d/1aJ8V1sNmRRQlqoV2LGr8DKZGSWGyLvjr/view?usp=sharing"
+                target="_blank"
+              >
+                Resume
+              </a>
+            </li>
+            {/* <!-- <li><a id="resume" href="https://drive.google.com/file/d/1Ma5AX5ldQhmVet3d2FS5ORqK6y7a4beX/view" target="_blank">Resume</a></li> --> */}
+          </ul>
+        </nav>
       </header>
     </div>
   );
